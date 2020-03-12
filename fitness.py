ffee11034506439ff16cd3ea759ffaa1f142f72d
Arekcze <arek.pawel.czerwinski@gmail.com>
2020-03-12 17:11:56 +0100

    worker class

--- fitness.py
+++ fitness.py
@@ -4,13 +4,15 @@ def fitness(board):
         for placeInd, place in enumerate(row):
             if place == '#':
                 newBoard[rowInd][placeInd] = "#"
-            elif(place == 'M'):
+            elif(place.kind == 'M'):
                 pass     
-            elif(place == "_"):
+            elif(place.kind == "_"):
                 ##creating submatrix
                 submatrix = test_slice(board,rowInd,placeInd)
-                
                 ## skills update for given cell
+                for rowsub in submatrix:
+                    for placesub in rowsub:
+                        calculatedSkillSum += placesub.skills
                 
             else:
                 print("we are screwed")
@@ -19,5 +21,14 @@ def fitness(board):
 def test_slice(m,i,j,slice_y = 3, slice_x = 3):
     sliceOfMatrix = [[m[a][b] for b in range(j, j + slice_x)] for a in range(i, i + slice_y)]
     return sliceOfMatrix
+
+class worker():
+    def __init__(self,kind,company,bonusPoints,potential,skills):
+        self.kind = kind
+        self.company = company
+        self.bonusPoints = bonusPoints
+        self.skills = skills
+
+
 
 
